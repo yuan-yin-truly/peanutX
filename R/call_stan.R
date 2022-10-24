9271b6bb799332258d9511e5d82dc9ed3737e806
yuan-yin-truly <76789185+yuan-yin-truly@users.noreply.github.com>
2022-10-24 17:32:22 -0400

    compile stan and add stan wrapper function

---
+++ R/call_stan.R
@@ -0,0 +1,19 @@
+#' Title
+#'
+#' @param data
+#' @param initial_condition
+#'
+#' @return
+#' @export
+#'
+#' @examples
+call_stan <- function(data, initial_condition){
+
+  out <- rstan::vb(object = stanmodels$shrinkage,
+                   init = initial_condition,
+                   data = data,
+                   seed = 12345,
+                   iter = 50000)
+
+  return(out)
+}
